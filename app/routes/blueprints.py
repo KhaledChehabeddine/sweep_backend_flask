2bd4de47c1fbc1eb12965fa7324c8923bfa5c9b1
Khaled Chehabeddine <khaled.ch2002@gmail.com>
2023-03-18 23:51:34 +0200

    Fixed routing initialization, and create function

--- app/routes/blueprints.py
+++ app/routes/blueprints.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 
-
-sweep_api_v1 = Blueprint(name='sweep_api_v1', import_name='sweep_api_v1', url_prefix='/api/v1/sweep')
-user_api_v1 = Blueprint(name='user_api_v1', import_name='user_api_v1', url_prefix='/user')
+sweep_api_v1 = Blueprint('sweep_api_v1', __name__, url_prefix='/api/v1/sweep')
+user_api_v1 = Blueprint('user_api_v1', __name__, url_prefix='/user')
 
 sweep_api_v1.register_blueprint(user_api_v1)
