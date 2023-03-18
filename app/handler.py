418c7051169040d0352d21218b19e2e9912e08fe
Khaled Chehabeddine <khaled.ch2002@gmail.com>
2023-03-18 23:36:54 +0200

    There is no god but God, and Muhammad is the Messenger of God

--- app/handler.py
+++ app/handler.py
@@ -9,7 +9,7 @@ def handler(event, context):
     method = event['requestContext']['http']['method']
     path = event['requestContext']['http']['path']
     with application.test_request_context(
-            path=path, method=method, headers=event['headers'], data=event.get('body', {})):
+            path=path, method=method, headers=event['headers'], data=event.get('body', "")):
         response = application.full_dispatch_request(request.json)
     return {
         'statusCode': response.status_code,
