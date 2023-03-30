01880ad4e3c32300577422fe4448b2bb4d8810a5
TestTesterThatTestsTestsTestTakersTake <jws09@mail.aub.edu>
2023-03-30 21:11:35 +0200

    jamil/account_subcategory

--- app/models/account_category_item.py
+++ app/models/account_category_item.py
@@ -1,58 +1,58 @@
-"""Summary: Account Category Item Model
+"""Summary: Account Main Category Item Model
 
-An account category item model used to convert an account category item document into an account category
+An account main category item model used to convert an account main category item document into an account main category
 item object
 """
 
 
-class AccountCategoryItem:
+class AccountMainCategoryItem:
     """
-    A class to represent an account category item model
+    A class to represent an account main category item model
 
 
     Attributes
     ----------
     icon : str
-        Account category item's icon
+        Account main category item's icon
     name : str
-        Account category item's name
+        Account main category item's name
 
     Methods
     -------
     get_icon() : str
-        Returns the account category item's icon
+        Returns the account main category item's icon
     set_icon(icon) : None
-        Sets the account category item's icon
+        Sets the account main category item's icon
     get_name() : str
-        Returns the account category item's name
+        Returns the account main category item's name
     set_name(name) : None
-        Sets the account category item's name
+        Sets the account main category item's name
     """
 
-    def __init__(self, account_category_item_document: dict) -> None:
-        self.icon = account_category_item_document['icon']
-        self.name = account_category_item_document['name']
+    def __init__(self, icon: str, name: str) -> None:
+        self.icon = icon
+        self.name = name
 
     def get_icon(self) -> str:
         """
-        :return: Account category item's icon
+        :return: Account main category item's icon
         """
         return self.icon
 
     def set_icon(self, icon: str) -> None:
         """
-        :param icon: Account category item's icon
+        :param icon: Account main category item's icon
         """
         self.icon = icon
 
     def get_name(self) -> str:
         """
-        :return: Account category item's name
+        :return: Account main category item's name
         """
         return self.name
 
     def set_name(self, name: str) -> None:
         """
-        :param name: Account category item's name
+        :param name: Account main category item's name
         """
         self.name = name
--- app/models/account_main_category.py
+++ app/models/account_main_category.py
@@ -4,7 +4,7 @@ An account main category model used to convert an account main category document
 """
 
 from typing import List
-from app.models.account_category_item import AccountCategoryItem
+from app.models.account_category_item import AccountMainCategoryItem
 
 
 class AccountMainCategory:
@@ -14,27 +14,27 @@ class AccountMainCategory:
 
     Attributes
     ----------
-    account_category_items : List[AccountMainCategoryItem]
+    account_main_category_items : List[AccountMainCategoryItem]
         Account main category's items
 
     Methods
     -------
-    get_account_category_items() : List[AccountCategoryItem]
+    get_account_main_category_items() : List[AccountMainCategoryItem]
         Returns the account main category's items
-    set_account_category_items(account_category_items) : None
+    set_account_main_category_items(account_main_category_items) : None
         Sets the account main category's items
     """
     def __init__(self, account_main_category_document: dict) -> None:
-        self.account_category_items = account_main_category_document['account_category_items']
+        self.account_main_category_items = account_main_category_document['account_main_category_items']
 
-    def get_account_category_items(self) -> List[AccountCategoryItem]:
+    def get_account_main_category_items(self) -> List[AccountMainCategoryItem]:
         """
         :return: Account main category's items
         """
-        return self.account_category_items
+        return self.account_main_category_items
 
-    def set_account_category_items(self, account_category_items: List[AccountCategoryItem]) -> None:
+    def set_account_main_category_items(self, account_main_category_items: List[AccountMainCategoryItem]) -> None:
         """
-        :param account_category_items: Account main category's items
+        :param account_main_category_items: Account main category's items
         """
-        self.account_category_items = account_category_items
+        self.account_main_category_items = account_main_category_items
