e1ebaa4d2abfc92ca68d833e42220c3e994b5e9c
TestTesterThatTestsTestsTestTakersTake <jws09@mail.aub.edu>
2023-04-03 23:23:54 +0300

    jamil/adding_home_database_support

--- app/models/homepage/home_sub_feature.py
+++ app/models/homepage/home_sub_feature.py
@@ -1,12 +1,50 @@
+"""Home sub feature model
+
+A home sub feature model used to convert a home sub feature document into a home sub feature object
 """
 
-"""
+
 from typing import List
 
 from app.models.homepage.home_sub_feature_item import HomeSubFeatureItem
 
 
 class HomeSubFeature:
+    """
+    A class to represent a home sub feature model
+
+
+    Attributes
+    ----------
+    sub_title : str
+        home sub feature's sub_title
+    title : str
+        home sub feature's title
+
+    home_sub_feature_items : List[HomeSubFeatureItem]
+        home sub feature's items
+
+    Methods
+    -------
+    get_home_sub_feature_items() : List[HomeSubFeatureItem]
+        Returns the home sub feature's items
+
+    set_home_sub_feature_items(home_sub_feature_items) : None
+        Sets the home sub feature's items
+
+    get_sub_title() : str
+        Returns the home sub feature's sub_title
+
+    set_sub_title(sub_title) : None
+        Sets the home sub feature's sub_title
+
+    get_title() : str
+
+        Returns the home sub feature's title
+
+    set_title(title) : None
+        Sets the home sub feature's title
+    """
 
     def __init__(self, home_sub_feature_document: dict) -> None:
         self.sub_title = home_sub_feature_document['sub_title']
@@ -28,7 +66,7 @@ class HomeSubFeature:
     def get_sub_title(self) -> str:
 
         """
-        :return: home sub feature's sub title
+        :return: home sub feature's sub_title
         """
         return self.sub_title
 
@@ -39,3 +77,7 @@ class HomeSubFeature:
         self.sub_title = sub_title
 
     def get_title(self) -> str:
+        """
+        :return: home sub feature's title
+        """
+        return self.title
