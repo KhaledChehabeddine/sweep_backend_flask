b027b5183a94d843c6d721c0070a473ad2fec6b0
Loza <blockerduude@gmail.com>
2023-05-01 21:09:58 +0300

    Finalized reservation controller

--- app/controllers/history/reservation_controller.py
+++ app/controllers/history/reservation_controller.py
@@ -4,12 +4,17 @@ A controller that assigns a child blueprint to sweep_api_v1 with routes for func
 delete reservations from the database
 """
 from datetime import datetime
-from flask import Blueprint, Response, request
-# from pymongo.errors import OperationFailure
+from flask import Blueprint, Response, request, jsonify
+from pymongo import ASCENDING
+from pymongo.errors import OperationFailure
 from app.aws.aws_s3_client import upload_image_to_aws_s3
+from app.database.database import get_database
 from app.models.history.reservation import Reservation
 
 raw_reservation_api_v1 = Blueprint('reservation_api_v1', __name__, url_prefix='/reservation')
+reservation_collection = get_database()['reservations']
+
+reservation_collection.create_index([('name', ASCENDING)], unique=True)
 
 
 def _configure_reservation_document(reservation_document: dict) -> dict:
@@ -35,27 +40,106 @@ def create_reservation() -> Response:
     """
 
     reservation_document = request.json
-    reservation_document['metadata'] = {}  # Missing metadata function
     reservation_document = _configure_reservation_document(reservation_document=reservation_document)
-    reservation_document = Reservation(reservation_document=reservation_document)
-    # try:
-    #   reservation_id = str(reservation_document.insert_one(reservation_document.database_dict()).inserted_id)
-    # except OperationFailure:
-    #    return jsonify(
-    #        message='Account category not added to the database.',
-    #        status=500
-    #    )
-    # return jsonify(
-    #   data=reservation_id,
-    #    message='Account category added to the database.',
-    #    status=200
-    # )
+    reservation = Reservation(reservation_document=reservation_document)
+    try:
+        reservation_id = str(reservation_collection.insert_one(reservation_document.database_dict()).inserted_id)
+    except OperationFailure:
+        return jsonify(
+            message='Account category not added to the database.',
+            status=500
+        )
+    return jsonify(
+        data=reservation_id,
+        message='Account category added to the database.',
+        status=200
+    )
 
 
 @raw_reservation_api_v1.route('/read/id/<string:_id>', methods=['GET'])
 def read_reservation_by_id(_id: str) -> Response:
     """
-    :param name: Reservation's id
-    :return: Response object with message if the reservation was found (if yes: add reservation) and the status code
+    :param _id: Reservation's id
+    :return: Response object with a message describing if the reservation was found (if yes: add reservation)
+    and the status code
     """
-    # reservation = _configure_reservation_document()
+    reservation_document = reservation_collection.find_one({'_id': _id})
+    if reservation_document is None:
+        return jsonify(
+            message='Reservation not found.',
+            status=404
+        )
+    reservation = Reservation(reservation_document=reservation_document)
+    return jsonify(
+        data=reservation.database_dict(),
+        message='Reservation found.',
+        status=200
+    )
+
+
+@raw_reservation_api_v1.route('/read/customer_id/<string:name>', methods=['GET'])
+def read_reservations_by_customer_id(_id: str) -> Response:
+    """
+    :param _id: Customer's id
+    :return: Response object with message if the customer's reservations were found (if
+    yes: add reservations) and the status code
+    """
+    reservation_documents = reservation_collection.find({'customer_id': _id})
+    if reservation_documents is None:
+        return jsonify(
+            message='Reservations not found.',
+            status=404
+        )
+    reservations = []
+    for reservation_document in reservation_documents:
+        reservation = Reservation(reservation_document=reservation_document)
+        reservations.append(reservation.database_dict())
+    return jsonify(
+        data=reservations,
+        message='Reservations found.',
+        status=200
+    )
+
+
+@raw_reservation_api_v1.route('/read', methods=['GET'])
+def read_reservations() -> Response:
+    """
+    :return: Response object with message if the reservations were found (if yes: add reservations) and the status code
+    """
+    reservation_documents = reservation_collection.find({})
+    if reservation_documents is None:
+        return jsonify(
+            message='Reservations not found.',
+            status=404
+        )
+    reservations = []
+    for reservation_document in reservation_documents:
+        reservation = Reservation(reservation_document=reservation_document)
+        reservations.append(reservation.database_dict())
+    return jsonify(
+        data=reservations,
+        message='Reservations found.',
+        status=200
+    )
+
+
+@raw_reservation_api_v1.route('/delete/id/<string:_id>', methods=['DELETE'])
+def delete_reservation_by_id(_id: str) -> Response:
+    """
+    :param _id: Reservation's id
+    :return: Response object with message if the reservation was deleted and the status code
+    """
+    reservation_document = reservation_collection.find_one({'_id': _id})
+    if reservation_document is None:
+        return jsonify(
+            message='Reservation not found.',
+            status=404
+        )
+    reservation_collection.delete_one({'_id': _id})
+    return jsonify(
+        message='Reservation deleted.',
+        status=200
+    )
+
+
+raw_reservation_api_v1 = raw_reservation_api_v1
