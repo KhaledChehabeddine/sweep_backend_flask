c85fb1386e7cf87afa9c8b8dcb04a081f3934337
hshibli2002 <99021546+hshibli2002@users.noreply.github.com>
2023-05-08 15:54:34 +0300

    Update search_category_controller.py

--- app/controllers/search/search_category_controller.py
+++ app/controllers/search/search_category_controller.py
@@ -32,12 +32,12 @@ def create_search_category() -> Response:
         search_category_id = str(search_category_collection.insert_one(search_category.database_document()).inserted_id)
     except errors.OperationFailure:
         return jsonify(
-            message='Search Category not added to the database.',
+            message='Search category not added to the database.',
             status=500
         )
     return jsonify(
         data=search_category_id,
-        message='Search Category added to the database.',
+        message='Search category added to the database.',
         status=200
     )
 
@@ -45,7 +45,7 @@ def create_search_category() -> Response:
 @raw_search_category_api_v1.route('/read/id/<string:_id>', methods=['GET'])
 def read_search_category_by_id(_id: str) -> Response:
     """
-    :param _id: Search Category's id
+    :param _id: Search category's id
     :return: Response object with a message describing if the search category was found (if yes: add search category
     objects) and the status code
     """
@@ -54,11 +54,11 @@ def read_search_category_by_id(_id: str) -> Response:
         search_category = SearchCategory(search_category_document=search_category_document)
         return jsonify(
             data=search_category.__dict__,
-            message='Search Category found in the database using the id.',
+            message='Search category found in the database using the id.',
             status=200,
         )
     return jsonify(
-        message='Search Category not found in the database using the id.',
+        message='Search category not found in the database using the id.',
         status=404
     )
 
@@ -66,7 +66,7 @@ def read_search_category_by_id(_id: str) -> Response:
 @raw_search_category_api_v1.route('/read/name/<string:category_name>', methods=['GET'])
 def read_search_category_by_name(category_name: str) -> Response:
     """
-    :param category_name: Search Category's name
+    :param category_name: Search category's name
     :return: Response object with a message describing if the search category was found (if yes: return search category
     objects) and the status code
     """
@@ -146,54 +146,10 @@ def update_search_category_by_id(_id: str) -> Response:
     )
 
 
-@raw_search_category_api_v1.route('/update/name/<string:category_name>', methods=['PUT'])
-def update_search_category_by_name(category_name: str) -> Response:
-    """
-    :param category_name: Search Category's name
-    :return: Response object with a message describing if the search category was updated (if yes: return search
-    category object) and the status code
-    """
-
-    search_category_document = request.json
-
-    search_category_document['metadata'] = \
-        create_search_category_metadata()
-
-    search_category = SearchCategory(search_category_document=search_category_document)
-
-    existing_category = search_category_collection.find_one({'category_name': category_name})
-    if not existing_category:
-        return jsonify(
-            message='Search Category not found in the database.',
-            status=404
-        )
-
-    try:
-        result = search_category_collection.update_one({'category_name': category_name},
-                                                       {'$set': search_category.database_document()})
-    except errors.OperationFailure:
-        return jsonify(
-            message='Search Category not updated in the database.',
-            status=500
-        )
-
-    if result.modified_count:
-        return jsonify(
-            data=search_category.__dict__,
-            message='Search Category updated in the database.',
-            status=200
-        )
-
-    return jsonify(
-        message='An error occurred while updating the search category.',
-        status=500
-    )
-
-
 @raw_search_category_api_v1.route('/delete/id/<string:_id>', methods=['DELETE'])
 def delete_search_category_by_id(_id: str) -> Response:
     """
-    :param _id: Search Category's id
+    :param _id: Search category's id
     :return: Response object with a message describing if the search category was deleted (if yes: add search category
     id) and the status code
     """
@@ -216,7 +172,7 @@ def delete_search_category_by_id(_id: str) -> Response:
         )
     except errors.OperationFailure:
         return jsonify(
-            message='Search Category not deleted from the database.',
+            message='Search category not deleted from the database.',
             status=500
         )
 
